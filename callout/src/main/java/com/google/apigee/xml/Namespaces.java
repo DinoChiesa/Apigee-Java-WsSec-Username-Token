ab1d4923554fc93b1e07b644bd75f7f4d211547b
Dino Chiesa <dchiesa@google.com>
2022-01-05 10:39:28 -0800

    proper config for antrun plugin
    - specify a clean phase
    - use target instead of tasks
    - also remove some compiler warnings

--- callout/src/main/java/com/google/apigee/xml/Namespaces.java
+++ callout/src/main/java/com/google/apigee/xml/Namespaces.java
@@ -1,3 +1,18 @@
+// Copyright 2019-2022 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.google.apigee.xml;
 
 import java.util.Collections;
@@ -8,26 +23,28 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 public class Namespaces {
-  public static final String WSU = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+  public static final String WSU =
+      "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
   public static final String SOAP10 = "http://schemas.xmlsoap.org/soap/envelope/";
-  public static final String WSSEC = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+  public static final String WSSEC =
+      "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
   public static final String XMLNS = "http://www.w3.org/2000/xmlns/";
   public static final String XMLDSIG = "http://www.w3.org/2000/09/xmldsig#";
 
-  public static final Map<String,String> defaultPrefixes;
+  public static final Map<String, String> defaultPrefixes;
 
   static {
-    HashMap map1 = new HashMap<String,String>();
-    map1.put(WSU,"wsu");
-    map1.put(SOAP10,"soap10");
-    map1.put(WSSEC,"wssec");
-    map1.put(XMLDSIG,"ds");
+    Map<String, String> map1 = new HashMap<String, String>();
+    map1.put(WSU, "wsu");
+    map1.put(SOAP10, "soap10");
+    map1.put(WSSEC, "wssec");
+    map1.put(XMLDSIG, "ds");
 
     defaultPrefixes = Collections.synchronizedMap(map1);
   }
 
-  public static Map<String,String> getExistingNamespaces(Element element) {
-    Map<String,String> knownNamespaces = new HashMap<String,String>();
+  public static Map<String, String> getExistingNamespaces(Element element) {
+    Map<String, String> knownNamespaces = new HashMap<String, String>();
     NamedNodeMap attributes = element.getAttributes();
     if (attributes != null) {
       for (int i = 0; i < attributes.getLength(); i++) {
@@ -41,8 +58,6 @@ public class Namespaces {
         }
       }
     }
-    return Collections.unmodifiableMap(knownNamespaces);  // key:namespace, value:prefix
+    return Collections.unmodifiableMap(knownNamespaces); // key:namespace, value:prefix
   }
-
-
 }
--- callout/src/test/java/com/google/apigee/callouts/wssecusernametoken/CalloutTestBase.java
+++ callout/src/test/java/com/google/apigee/callouts/wssecusernametoken/CalloutTestBase.java
@@ -1,5 +1,16 @@
-// CalloutTestBase.java
-// ------------------------------------------------------------------
+// Copyright © 2019-2022 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 package com.google.apigee.callouts.wssecusernametoken;
@@ -31,24 +42,24 @@ public abstract class CalloutTestBase {
 
     msgCtxt =
         new MockUp<MessageContext>() {
-          private Map variables;
+          private Map<String,Object> variables;
 
           public void $init() {
-            variables = new HashMap();
+            variables = new HashMap<String,Object>();
           }
 
           @Mock()
-          public <T> T getVariable(final String name) {
+          public Object getVariable(final String name) {
             if (variables == null) {
-              variables = new HashMap();
+              variables = new HashMap<String,Object>();
             }
-            return (T) variables.get(name);
+            return variables.get(name);
           }
 
           @Mock()
           public boolean setVariable(final String name, final Object value) {
             if (variables == null) {
-              variables = new HashMap();
+              variables = new HashMap<String,Object>();
             }
             System.out.printf("setVariable(%s, %s)\n", name, value.toString());
             variables.put(name, value);
@@ -58,7 +69,7 @@ public abstract class CalloutTestBase {
           @Mock()
           public boolean removeVariable(final String name) {
             if (variables == null) {
-              variables = new HashMap();
+              variables = new HashMap<String,Object>();
             }
             if (variables.containsKey(name)) {
               variables.remove(name);
